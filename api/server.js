0027aa0da9a8c0ceee4994ee7c47f39f078765f7
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-15 19:25:10 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
+const accountsRouter = require('./accounts/accounts-router');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+// everything that starts with accounts will go to this accountsRouter. 
+
+//This has to be the last one 
 
 server.use('*', (req, res) => {
     res.status(404).json({
