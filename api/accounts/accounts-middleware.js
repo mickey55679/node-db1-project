aee1e5cfdf092fecdeeef59453ad82ebd334cb1e
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-16 22:35:26 -0500

    call next on middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,12 +2,15 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  next() // allow request to proceed
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+
+ next()
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  next()
 }
