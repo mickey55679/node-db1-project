cc9e37dbf46b3deeba04804be2f7a709015f4013
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-17 18:31:54 -0500

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,9 +2,25 @@ const Account = require('./accounts-model')
 
 
 exports.checkAccountPayload = (req, res, next) => {
-
-  console.log('checkAccountPayload middleware')
-  next() // allow request to proceed
+const error = {status: 400}
+const {name, budget} = req.body
+if (name === undefined || budget === undefined) {
+ error.message = "name and budget are required"
+ next(error)
+} else if (typeof name !== 'string'){
+  error.message = "name of account must be a string"
+  next(error)
+} else if (name.trim().length < 3 || name.trim().length > 100) {
+  error.message = "name of account must be between 3 and 100"
+  next(error)
+} 
+else if (typeof budget !== 'number' || !isNaN(budget)) {
+  error.message = "budget of account must be a number"
+ next(error)
+}
+else if(){
+  
+}
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
