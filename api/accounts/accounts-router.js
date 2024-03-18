c008bccfe0fad138f44d5b2b254006a9f0a216a1
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-17 19:17:08 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -19,9 +19,10 @@ router.get('/:id', md.checkAccountId, async (req, res, next) => {
 router.post('/',
  md.checkAccountPayload, 
  md.checkAccountNameUnique,
-  (req, res, next) => {
+ async (req, res, next) => {
   try {
-    res.json()
+    const newAccount = await Account.create(req.body)
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err);
   }
