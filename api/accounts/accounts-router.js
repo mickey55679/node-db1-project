b34268793780df7dfab25e13981d2bd25a6d7927
Michaiah Bos <michaiahbos@yahoo.com>
2024-03-17 19:59:29 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,59 +1,64 @@
-const router = require('express').Router()
-const Account = require('./accounts-model')
-const md = require('./accounts-middleware');
+const router = require("express").Router();
+const Account = require("./accounts-model");
+const md = require("./accounts-middleware");
 
-router.get('/', async  (req, res, next) => {
-  try{
-  const accounts = await Account.getAll()
-  res.json(accounts)
-  } catch(err){
-    next(err)
-
-  }
-})
-
-router.get('/:id', md.checkAccountId, async (req, res, next) => {
- res.json(req.account)
-})
-
-router.post('/',
- md.checkAccountPayload, 
- md.checkAccountNameUnique,
- async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const newAccount = await Account.create(req.body)
-    res.status(201).json(newAccount)
-  } catch (err) {
-    next(err);
-  }
-
-})
-
-router.put('/:id',
- md.checkAccountId, 
- md.checkAccountPayload, 
- md.checkAccountNameUnique,
-  (req, res, next) => {
-  
-  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
   } catch (err) {
     next(err);
   }
 });
 
-router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+router.get("/:id", md.checkAccountId, async (req, res, next) => {
+  res.json(req.account);
+});
 
+router.post(
+  "/",
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const newAccount = await Account.create({ 
+        name: req.body.name.trim(),
+        budget: req.body.budget,
+       });
+      res.status(201).json(newAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountPayload,
+  async (req, res, next) => {
+    const updated = await Account.updateById(req.params.id, req.body);
+    res.json(updated);
+    try {
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", md.checkAccountId, async (req, res, next) => {
   try {
-    await Account.deleteById(req.params.id)
+    await Account.deleteById(req.params.id);
     res.json(req.account);
   } catch (err) {
     next(err);
   }
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
- message: err.message, 
+    message: err.message,
   });
 });
 
